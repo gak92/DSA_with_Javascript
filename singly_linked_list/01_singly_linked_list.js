6a0f012321f293bd2118919e5dfcfa161697a4e4
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-03 19:46:07 +0500

    Add methods: traverse and push

--- singly_linked_list/01_singly_linked_list.js
+++ singly_linked_list/01_singly_linked_list.js
@@ -11,5 +11,36 @@ class SinglyLinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  traverse() {
+    let current = this.head;
+    let arr = [];
+    while(current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log("List is: ", arr);
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    }
+    else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
 }
+
+let list = new SinglyLinkedList();
+list.push(100);
+list.push(200);
+list.push(300);
+list.traverse();
+
 
