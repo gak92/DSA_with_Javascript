d4a0f49b04c19a6c179d0a2effdea79029147f26
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-04 22:50:02 +0500

    Add push method of doubl linked list

--- doubly_linked_list/01_doubly_linked_list.js
+++ doubly_linked_list/01_doubly_linked_list.js
@@ -12,4 +12,29 @@ class DoublyLinkedList {
     this.head = null;
     this.tail = null;
   }
+
+  push(val) {
+    let newNode = new Node(val);
+    if(!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    }
+    else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length += 1;
+    return this;
+  }
 }
+
+let list = new DoublyLinkedList();
+
+// Push an element
+list.push(100);
+list.push(200);
+list.push(300);
+list.push(400);
+list.push(500);
+// list.traverse();
