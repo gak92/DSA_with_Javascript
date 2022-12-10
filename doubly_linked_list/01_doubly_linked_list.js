3f4e2a90a48900bb9bf9447aae198ed9e3f6e43a
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-10 12:58:42 +0500

    Add remove  method of doubly linked list

--- doubly_linked_list/01_doubly_linked_list.js
+++ doubly_linked_list/01_doubly_linked_list.js
@@ -143,6 +143,21 @@ class DoublyLinkedList {
     return true;
   }
 
+  remove(index) {
+    if(index < 0 || index >= this.length) return undefined;
+    if(index === 0) return this.shift();
+    if(index === this.length-1) return this.pop();
+
+    let removedNode = this.get(index);
+    removedNode.prev.next = removedNode.next;
+    removedNode.next.prev = removedNode.prev;
+
+    removedNode.next = null;
+    removedNode.prev = null;
+    this.length -= 1;
+    return removedNode;
+  }
+
 } // End of class
 
 let list = new DoublyLinkedList();
@@ -178,5 +193,9 @@ list.traverse();
 // list.traverse();
 
 // Insert an element
-list.insert(0, 141);
+// list.insert(0, 141);
+// list.traverse();
+
+// Remove an element
+list.remove(2);
 list.traverse();
