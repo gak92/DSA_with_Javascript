c5306cab7f6d108ee62cecd818fda414a53fa9ff
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-11 02:22:25 +0500

    Implement MaxBinaryHeap extractMax method

--- Trees/04_Max_Binary_Heap.js
+++ Trees/04_Max_Binary_Heap.js
@@ -22,6 +22,49 @@ class MaxBinaryHeap {
       idx = parentIdx;
     }
   }
+
+  extractMax(){
+    const max = this.values[0];
+    let end = this.values.pop();
+    if(this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[idx];
+
+    while(true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if(leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if(leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+
+      if(rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)) {
+          swap = rightChildIdx;
+        }
+      }
+
+      if(swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    } // End of while loop
+  } // End of sinkDown method
+
 } // End of class MaxBinarHeap
 
 
@@ -32,3 +75,9 @@ heap.insert(55);
 console.log("After insert 55: ", heap.values);
 heap.insert(11);
 console.log("After insert 11: ", heap.values);
+
+// Extract Max value
+console.log("Max value: ", heap.extractMax());
+console.log("After extract max value: ", heap.values);
+console.log("Max value: ", heap.extractMax());
+console.log("After extract max value: ", heap.values);
