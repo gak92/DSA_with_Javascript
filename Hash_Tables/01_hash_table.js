ca6ee6d3730584b6957435cd768d253570288613
gak92 <44802922+gak92@users.noreply.github.com>
2022-12-11 15:36:15 +0500

    Add hash table with set and get method

---
+++ Hash_Tables/01_hash_table.js
@@ -0,0 +1,54 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    const PRIME = 31;
+
+    for(let i=0; i < Math.min(key.length, 100); i++) {
+      let ch = key[i];
+      let val = ch.charCodeAt(0) - 96;
+      total = (total * PRIME + val) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, val) {
+    let index = this._hash(key);
+    if(!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, val]);
+  }
+
+  get(key) {
+    let index = this._hash(key);
+    if(this.keyMap[index]) {
+      for(let i=0; i<this.keyMap[index].length; i++) {
+        if(this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+
+    return undefined;
+  }
+
+} // End of class HashTable
+
+let ht = new HashTable(11);
+ht.set("maroon","#800000")
+ht.set("yellow","#FFFF00")
+ht.set("olive","#808000")
+ht.set("salmon","#FA8072")
+ht.set("lightcoral","#F08080")
+ht.set("mediumvioletred","#C71585")
+ht.set("plum","#DDA0DD")
+ht.set("purple","#DDA0DD")
+ht.set("violet","#DDA0DD")
+
+console.log(ht.keyMap);
+console.log(ht.get("salmon"));
+console.log(ht.get("yellow"));
